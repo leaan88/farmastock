516f8dffde3905424cea0e4f64cb730ccc40c75a
Leandro <83724929+leaan88@users.noreply.github.com>
2024-12-05 17:50:50 -0300

    Delete Farmacia/project/src/hook directory

--- Farmacia/project/src/hook/useAuth.js
+++
@@ -1,9 +0,0 @@
-import create from 'zustand';
-
-const useAuth = create((set) => ({
-  user: null,
-  login: (userData) => set({ user: userData }),
-  logout: () => set({ user: null }),
-}));
-
-export { useAuth };
